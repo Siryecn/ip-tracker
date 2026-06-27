545ada755765f9e822bd2cafcb76978fb4edcf58
Siryecn <siryecn@gmail.com>
2026-06-27 09:48:07 +0800

    Update run_iptanzhen.bat

--- run_iptanzhen.bat
+++ run_iptanzhen.bat
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001 >nul
-echo 🎉 正在启动Cpolar全自动更新工具（终极版）...
+echo 正在启动Cpolar全自动更新工具
 echo ================================================
 :: 获取当前 bat 文件所在目录，自动切换到该目录
 cd /d "%~dp0"
@@ -8,5 +8,5 @@ cd /d "%~dp0"
 set PYTHONIOENCODING=utf-8
 :: 使用虚拟环境运行 Python 脚本
 start "Cpolar更新工具" /MIN .\venv\Scripts\python.exe "Upgrade-ip.py"
-:: 当前bat窗口立即退出，无任何残留
+:: 当前bat窗口立即退出
 exit
